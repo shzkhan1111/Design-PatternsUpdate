5fe1cc39432ccd78e091292badc6045279118b18
shzkhan1111 <shahzaib.ahmedkhan111@gmail.com>
2024-10-21 21:08:28 +0500

    Factory Basic Complete

--- Design Patterns/FactoryDesignPattern.cs
+++ Design Patterns/FactoryDesignPattern.cs
@@ -17,9 +17,16 @@ namespace Design_Patterns
     {
         void Drive();
         void Honk();
+        public string typeOfCar { get; set; }
     }
     public class Carolla : ICar
     {
+        public Carolla()
+        {
+            typeOfCar = "carolla";
+        }
+        public string typeOfCar { get; set; }
+
         public void Drive()
         {
             Console.WriteLine("Driving Caroola");
@@ -29,10 +36,18 @@ namespace Design_Patterns
         {
             Console.WriteLine("Caroola's Honk");
         }
+
     }
 
     public class Sidan : ICar
     {
+        public Sidan()
+        {
+            typeOfCar = "Sidan";
+        }
+        public string typeOfCar { get; set; }
+
+        
         public void Drive()
         {
             Console.WriteLine("Driving Sidan");
@@ -46,5 +61,70 @@ namespace Design_Patterns
 
     //now we willl create a factory to create a instace of car on run 
     //calling code can change the variant on run time 
+    public interface ICarFactory
+    {
+        //a method to return a new instance of car
+        ICar CreateCar();
+    }
+    // create a factory for Sidan and carolla
+    public class SidanFactory : ICarFactory
+    {
+        public ICar CreateCar()
+        {
+            return new Sidan();
+        }
+    }
+
+    public class CarollaFactory : ICarFactory
+    {
+        public ICar CreateCar()
+        {
+            return new Carolla();
+        }
+    }
+
+    public class CarClient
+    {
+        //client Get teh Instace of child and it depends on run time
+        private readonly ICar _car;
+        public CarClient(ICarFactory carfactory)
+        {
+            //call the factory method to get the car
+            _car = carfactory.CreateCar();
+        }
+        public void Drive()
+        {
+            _car.Drive();
+        }
+        public void Honk()
+        {
+            _car.Honk();
+        }
+        public void getType()
+        {
+            Console.WriteLine(_car.typeOfCar);
+        }
+    }
+
+    public class FactoryPAtternMainProcess
+    {
+        public void ExecMain()
+        {
+            ICarFactory sedanFactory = new SidanFactory();
+            ICarFactory carollaFactory = new CarollaFactory();
+            CarClient carClient = new CarClient(sedanFactory);
+            CarClient carClient1 = new CarClient(carollaFactory);
+
+            carClient.Drive();
+            carClient.Honk();
+            carClient.getType();
+
+
+            carClient1.Drive();
+            carClient1.Honk();
+            carClient1.getType();
+
+        }
+    }
 
 }
--- Design Patterns/Program.cs
+++ Design Patterns/Program.cs
@@ -1,1 +1,3 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Design_Patterns;
+
+new FactoryPAtternMainProcess().ExecMain();
